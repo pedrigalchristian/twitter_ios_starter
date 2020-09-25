9641ce15ce8404a17b2a1256fd383b92e1680d71
Christian Pedrigal <pedrigalchristian@gmail.com>
2020-09-24 23:06:39 -0700

    bruh

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -23,7 +23,7 @@ class HomeTableTableViewController: UITableViewController {
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count":numberOfTweet]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
              self.tweetArray.removeAll()
             for tweet in tweets{
@@ -39,6 +39,7 @@ class HomeTableTableViewController: UITableViewController {
         })
     }
     
+
     
     
     func loadMoreTweets(){
@@ -50,7 +51,7 @@ class HomeTableTableViewController: UITableViewController {
         let myParams = ["count": numberOfTweet]
         
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
              self.tweetArray.removeAll()
             for tweet in tweets{
@@ -67,6 +68,9 @@ class HomeTableTableViewController: UITableViewController {
     }
     
     
+    
+    
+    
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
         
         if indexPath.row + 1 == tweetArray.count {
@@ -74,14 +78,26 @@ class HomeTableTableViewController: UITableViewController {
         }
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
+        self.tableView.reloadData()
+    
+        
     }
 
     @IBAction func onLogout(_ sender: Any) {
+        
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
@@ -106,6 +122,10 @@ class HomeTableTableViewController: UITableViewController {
         if let imageData = data {
                 cell.profileImageView.image = UIImage(data:imageData)
             }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
